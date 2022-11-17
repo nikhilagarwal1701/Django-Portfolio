93097f4cec80d8c10278e1ac41ef215ba841c8c1
Nikhil Agarwal <nagarwal1701@gmail.com>
2022-11-18 04:47:36 +0530

    set up resume download

--- portfolio/software/views.py
+++ portfolio/software/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Resume
 # Create your views here.
 
 def homepage(request):
     return render(request, "software/homepage.html")
 
 def resume(request):
-    return render(request, 'software/resume.html')
+    return render(request, 'software/resume.html', {'resumes': Resume.objects.all})
