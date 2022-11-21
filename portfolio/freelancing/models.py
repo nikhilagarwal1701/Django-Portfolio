cc293f779adf51a84128c098d23e31ae6308c4d2
Nikhil Agarwal <nagarwal1701@gmail.com>
2022-11-22 05:01:46 +0530

    Resume download button and URL

--- portfolio/freelancing/models.py
+++ portfolio/freelancing/models.py
@@ -7,8 +7,6 @@ class Wallpaper(models.Model):
     wallpaper_name = models.CharField(max_length=30)
     wallpaper_image = models.ImageField(upload_to='main/wallpapers', default= 'wallpaper')
     wallpaper_drive_url = models.URLField(default="")
-    #Original URL: https://drive.google.com/file/d/0B6wwyazyzml-OGQ3VUo0Z2thdmc/view
-    #Use URL: https://drive.google.com/uc?export=view&id=0B6wwyazyzml-OGQ3VUo0Z2thdmc
     wallpaper_date = models.DateTimeField("Date Published")
 
 
