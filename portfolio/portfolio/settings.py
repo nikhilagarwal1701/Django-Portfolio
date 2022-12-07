ae534f2990bdda5e316f02f783fe7ea69fb769e2
Nikhil Agarwal <nagarwal1701@gmail.com>
2022-12-08 02:17:52 +0530

    Revert "deployment changes"
    This reverts commit df3fd172227bb6a669d5ef0fbb56211881dbb668.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=8*$9v=t1e*#7h(_^^aj#@u*8h^6l7()&@8=b_7zn5uura=hxf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'portfolioDjango.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
